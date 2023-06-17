bbd03342525030e2bb5a3825d2b01d668cd31941
Joshua Gregory <115691670+joshtype@users.noreply.github.com>
2023-06-17 16:04:51 -0400

    Update fibonacci.js
    Improved memory/readability.

--- fibonacci.js
+++ fibonacci.js
@@ -1,54 +1,67 @@
 /**
- * Iterative Fibonacci function in JavaScript.
+ * Iterative Fibonacci functions
  * Formula: F(n) = F(n-1) + F(n-2) where n > 1.
  * F(0) = 0, F(1) = 1 (not technically valid inputs).
  */
 
-// individual fibonacci number
-function fibo(n) {
+/**
+ * Determines the Fibonacci number from n.
+ * @param  {number} n
+ * @return {number} Fibonacci number of n
+ */
+const nextFibo = function(n) {
     if(n > 1) {
         return (n-1) + (n-2);
     } else {
         return n;
     }
-}
+};
 
-// sequence of 15 fibonacci numbers from n using iteration
-function iterFibo(n) {
-    // array container for sequence of 10 nums
+/**
+ * Function iteratively sequences 15
+ * Fibonacci numbers starting from n.
+ * @param  {number} n
+ * @return {numbers[]} array of Fibonacci numbers
+ */
+const arrFibo = function(n) {
+    // declare arr to store fibo nums
     var seq = [];
-    // F(0) = 0, F(1) = 1
+
+    // set idx 0 to 0 & idx 1 to 1 if n = 0
     if(n == 0) {
+        // F(0) = 0
         seq[0] = 0;
+        // F(1) = 1
         seq[1] = 1;
-        // iteratively determine following 13 fibo nums
+        // fill arr with following 13 fibo nums
         for(i = 2; i < 15; i++) {
             seq[i] = (n-1) + (n-2);
             n++;
         }
-    }
-    // F(1) = 1
-    if(n == 1) {
+    // set idx 0 to 1 if n = 1
+    } else if(n == 1) {
+        // F(1) = 1
         seq[0] = 1;
-        // iteratively determine following 14 fibo nums
+        // fill arr with following 14 fibo nums
         for(i = 1; i < 15; i++) {
             seq[i] = (n-1) + (n-2);
         }
-     }
-     // input > 1
-     if(n > 1) {
-        // iteratively determine all 15 fibo nums
+     // true fibo sequence
+     } else if(n > 1) {
+        // fill arr with all 15 fibo nums
         for(i = 0; i < 15; i++) {
             seq[i] = (n-1) + (n-2);
             n++;
         }
     }
-     // display fibo nums
-     for(i = 0; i < seq.length; i++) {
-        console.log(seq[i]);
-     }
-}
+    // return arr of 15 fibo nums
+    return seq;
+};
 
-console.log("Finding an individual Fibonacci number: " + fibo(151));
-console.log("Finding sequence of 15 Fibonacci numbers iteratively: ");
-iterFibo(12);
+// define test parameters
+let n1 = 151;
+let n2 = 12;
+
+// invoke functions to test
+console.log("The Fibonacci number of " + n1 + " = " + nextFibo(n1));
+console.log("Sequence of 15 Fibonacci numbers from " + n2 + " = " + arrFibo(n2));
